53921b92496ceca15d10fcfca025773fb8bd9d8d
Mohammad Momeni <drmohammadmomeni70@yahoo.com>
2023-04-15 14:45:54 +0330

    Create a banner

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import Banner from '@/components/Banner';
+
 export default function Home() {
   return (
-    <div className="w-[100dvw] h-[150dvh] text-center pt-80 bg-slate-400 text-white">
-      <span className="text-4xl">Hello World</span>
+    <div className="w-[100dvw] h-[150dvh] text-center bg-slate-400 text-white">
+      <Banner />
     </div>
   );
 }
